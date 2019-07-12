afb9475eda9bb2aa70a55cfff3c1fabc65db6d5f
Alexander Abushady <aabushady214@gmail.com>
2019-07-12 13:20:47 -0400

    Implemented Table View.
    Implemented the tableView and hardcoded it to be 10 rows with each cell saying "Hello".

--- TODO: List/ViewController.swift
+++ TODO: List/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // These make the tableview look at the view controller for what content and how many cells should be added.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    // This function determines how many rows are in the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    // This function determines what content is in the rows.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Constant that assigns to a cell. This allows for manipulation of content.
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
 }
 
