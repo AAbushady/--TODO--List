6449c76c4a58965489ad849848e31655c0387459
Alexander Abushady <aabushady214@gmail.com>
2019-07-14 14:01:28 -0400

    Implemented Task Class
    Created the Task.swift file and the Task class.  Implemented an array that takes objects from the Task class to display as cells.

---
+++ TODO: List/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  TODO: List
+//
+//  Created by Alexander Abushady on 7/14/19.
+//  Copyright © 2019 Alexander Abushady. All rights reserved.
+//
+
+/*
+ This classs is to create each task within the To do list.
+ */
+
+import Foundation
+
+// The Task class is used to instantiate class objects.
+class Task {
+    var name = ""
+    var important = false
+}
--- TODO: List/ViewController.swift
+++ TODO: List/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    // Creates the empty array tasks of type Task.
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // This allows us to fill the array with the tasks from makeTasks.
+        tasks = makeTasks()
         
         // These make the tableview look at the view controller for what content and how many cells should be added.
         tableView.dataSource = self
@@ -22,15 +28,42 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     // This function determines how many rows are in the table view.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        // Returning the count of tasks array allows for the task array to dictate how many rows there will be.
+        return tasks.count
     }
     // This function determines what content is in the rows.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Constant that assigns to a cell. This allows for manipulation of content.
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task  = tasks[indexPath.row]
+        if task.important {
+            // Set each cell to have its corresponding task name as the content.
+            // The if statement will set the task to have an exclamation point if task.important = true.
+            cell.textLabel?.text = "\(task.name)❗️"
+        } else {
+            // Set each cell to have its corresponding task name as the content.
+            cell.textLabel?.text = task.name
+        }
         return cell
     }
+    
+    // This function returns an array of type task.
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false;
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true;
+        
+        let task3 = Task()
+        task3.name = "Mow the Lawn"
+        task3.important = false;
+        
+        return [task1, task2, task3]
+    }
+    
     
 }
 
